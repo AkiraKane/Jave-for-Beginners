0e433425e663b77eac268ae1892cc406356930a3
xiandalong <kelvinlong8897@gmail.com>
2014-12-15 09:42:04 -0500

    12.14.2014

---
+++ Tutorial47/src/WriteObjects.java
@@ -0,0 +1,34 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class WriteObjects {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Writing objects...");
+		
+		
+		try(ObjectOutputStream os =new ObjectOutputStream(new FileOutputStream("test.ser"))){
+			
+			
+			Person person = new Person(7,"Bob");
+			Person.setCount(88);
+			os.writeObject(person);
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
